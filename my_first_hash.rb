7ec121cd2cc471f7a5d88562ddb7953aaa6bfef5
Matt Smith <mrandolph83@gmail.com>
2020-10-14 18:07:26 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,12 +1,12 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = {"name" => "Matt", "dog" => "Dori"}
+  your_hash = {"name" => "Matt", "dog" => "Dori"}
 
 end
 
 
 def shipping_manifest
-  shipping_manifest = {
+  the_manifest = {
     "whale bone corsets" => 5,
     "porcelain vases" => 2,
     "oil paintings" => 3
@@ -25,7 +25,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-shipping_manifest["oil pantings"]
+shipping_manifest["oil paintings"]
 end
 
 def adding
